b33bcefe835bb5924f3d3f147462b97d2309d818
fan23j <fan23j@gmail.com>
2023-03-31 23:27:32 -0400

    extend conversion script to full body keypoints

--- utils/coco2yolo.py
+++ utils/coco2yolo.py
@@ -1,13 +1,11 @@
 #COCO 格式的数据集转化为 YOLO 格式的数据集
 #--json_path 输入的json文件路径
 #--save_path 保存的文件夹名字，默认为当前目录下的labels。
- 
 import os
 import json
 from tqdm import tqdm
 import argparse
 import numpy as np
- 
 parser = argparse.ArgumentParser()
 #这里根据自己的json文件位置，换成自己的就行
 parser.add_argument('--json_path', default='coco_kpts/annotations/instances_val2017.json',type=str, help="input: coco format(json)")
@@ -16,8 +14,7 @@ parser.add_argument('--save_path', default='coco_kpts/labels/val2017', type=str,
 parser.add_argument('--split', default='train', type=str,
 help="specify train/val split")
 arg = parser.parse_args()
- 
-def convert(size, box, keypoints):
+def convert(size, box, keypoints, width, height):
     dw = 1. / (size[0])
     dh = 1. / (size[1])
     x = box[0] + box[2] / 2.0
@@ -29,22 +26,21 @@ def convert(size, box, keypoints):
     w = round(w * dw, 6)
     y = round(y * dh, 6)
     h = round(h * dh, 6)
-    
-    keypoint = keypoints.copy()
-    keypoint[::3] = np.array(keypoint[::3]) / w
-    keypoint[1::3] = np.array(keypoint[1::3]) / h
-    
-    return (x, y, w, h), keypoint
- 
+    kpt_sets = []
+    # normalize [keypoints, foot_kpts, face_kpts, lefthand_kpts, righthand_kpts]
+    for kpt_set in keypoints:
+        kpt_set.copy()
+        kpt_set[::3] = np.array(kpt_set[::3]) / width
+        kpt_set[1::3] = np.array(kpt_set[1::3]) / height
+        kpt_sets.append(kpt_set)
+    return (x, y, w, h), list(np.concatenate(kpt_sets).flat)
 if __name__ == '__main__':
     json_file =   arg.json_path # COCO Object Instance 类型的标注
     ana_txt_save_path = arg.save_path  # 保存的路径
     split = arg.split
- 
     data = json.load(open(json_file, 'r'))
     if not os.path.exists(ana_txt_save_path):
         os.makedirs(ana_txt_save_path)
- 
     id_map = {} # coco数据集的id不连续！重新映射一下再输出！
     with open(os.path.join(ana_txt_save_path, 'classes.txt'), 'w') as f:
         # 写入classes.txt
@@ -64,11 +60,10 @@ if __name__ == '__main__':
         f_txt = open(os.path.join(ana_txt_save_path, ana_txt_name), 'w')
         for ann in data['annotations']:
             if ann['image_id'] == img_id:
-                box, keypoints = convert((img_width, img_height), ann["bbox"], ann["keypoints"])
+                box, keypoints = convert((img_width, img_height), ann["bbox"], [ann["keypoints"], ann["foot_kpts"], ann["face_kpts"], ann["lefthand_kpts"], ann["righthand_kpts"]], img_width, img_height)
                 line = (id_map[ann["category_id"]], *box, *keypoints)
-                #f_txt.write("%s %s %s %s %s\n" % (id_map[ann["category_id"]], box[0], box[1], box[2], box[3]))
                 f_txt.write(('%g ' * len(line)).rstrip() % line + '\n')
         f_txt.close()
         #将图片的相对路径写入train2017或val2017的路径
-        list_file.write('./%s/%s.jpg\n' %(split,head))
+        list_file.write('./images/%s/%s.jpg\n' %(split,head))
     list_file.close()
